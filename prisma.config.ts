790f9d1822ba5ac2612f09784074507f591dcf7f
Roman <roman.developer@mail.ru>
2026-04-08 10:58:39 +0300

    ci(update-release-version): fix installation dependencies

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,6 +5,9 @@ dotenv.config({
   path: `${__dirname}/.env.${process.env.NODE_ENV === 'production' ? 'production' : 'development'}`,
 });
 
+console.log('hello');
+console.log(env('DATABASE_URL'), process.env.NODE_ENV);
+
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: {
