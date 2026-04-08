5d851b07323bde5a4d9770767d9636e67c5e1ab4
Roman <roman.developer@mail.ru>
2026-04-08 11:30:29 +0300

    ci(*): fix install deps step

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,14 +1,9 @@
 import dotenv from 'dotenv';
 import { defineConfig, env } from 'prisma/config';
 
-const abc = {};
-
 dotenv.config({
-  processEnv: abc,
   path: `${__dirname}/.env.${process.env.NODE_ENV === 'production' ? 'production' : 'development'}`,
 });
-
-console.log(process.env.NODE_ENV, typeof process.env.DATABASE_URL, process.env.DATABASE_URL, abc);
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
