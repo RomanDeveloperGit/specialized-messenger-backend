d2f73f0acff8bb28a00e235b158446a3d2794f32
Roman <roman.developer@mail.ru>
2026-04-13 15:41:23 +0300

    perf(indexes): add migration file

---
+++ prisma/migrations/20260413124040_indexes/migration.sql
@@ -0,0 +1,8 @@
+-- CreateIndex
+CREATE INDEX "ConversationParticipant_userId_idx" ON "ConversationParticipant"("userId");
+
+-- CreateIndex
+CREATE INDEX "Message_conversationId_createdAt_idx" ON "Message"("conversationId", "createdAt");
+
+-- CreateIndex
+CREATE INDEX "Message_senderId_idx" ON "Message"("senderId");
