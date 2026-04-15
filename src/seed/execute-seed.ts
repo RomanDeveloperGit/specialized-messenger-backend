d89837fb134fa040f91e90efb7010c776347d960
Roman <roman.developer@mail.ru>
2026-04-15 11:26:28 +0300

    chore(*): fix seed script

--- src/seed/execute-seed.ts
+++ src/seed/execute-seed.ts
@@ -1,5 +1,8 @@
 import { NestFactory } from '@nestjs/core';
+import { EventEmitter2 } from '@nestjs/event-emitter';
 
+import { ChatService } from '@/modules/chat/chat.service';
+import { User } from '@/modules/user/dto/user.dto';
 import { UserService } from '@/modules/user/user.service';
 
 import { PrismaService } from '@/shared/modules/prisma';
@@ -9,22 +12,54 @@ import { AppModule } from '@/app.module';
 import { USER_MOCKS } from './mocks/users';
 
 export const executeSeed = async () => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  EventEmitter2.prototype.emit = () => {};
+
   const app = await NestFactory.createApplicationContext(AppModule);
   const prismaService = app.get(PrismaService);
 
-  await prismaService.$transaction([
-    prismaService.invitation.deleteMany(),
-    prismaService.message.deleteMany(),
-    prismaService.conversationParticipant.deleteMany(),
-    prismaService.conversation.deleteMany(),
-    prismaService.user.deleteMany(),
-  ]);
+  await prismaService.$executeRawUnsafe(`
+    DO $$ DECLARE
+      r RECORD;
+    BEGIN
+      FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') LOOP
+        EXECUTE 'TRUNCATE TABLE "' || r.tablename || '" RESTART IDENTITY CASCADE';
+      END LOOP;
+    END $$;
+  `);
 
   const userService = app.get(UserService);
+  const chatService = app.get(ChatService);
+
+  const users: User[] = [];
 
   for (const user of USER_MOCKS) {
-    await userService.create(user);
+    users.push(await userService.create(user));
   }
+
+  const conversation = await chatService.createConversation(users[0].id, {
+    type: 'DIRECT',
+    participantUserIds: [users[1].publicId],
+  });
+
+  await chatService.createMessage({
+    conversationId: conversation.id,
+    authorUserId: users[0].id,
+    type: 'TEXT',
+    content: {
+      text: 'Передаю привет от админа!',
+    },
+  });
+
+  await chatService.createMessage({
+    conversationId: conversation.id,
+    authorUserId: users[1].id,
+    type: 'TEXT',
+    content: {
+      text: 'Принимаю привет от админа!',
+    },
+  });
 
   console.log('Seeding complete!');
 
