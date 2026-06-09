32647e8bf095b1cd02ce4d56a4467eee5ae5acae
Roman <roman.developer@mail.ru>
2026-06-09 21:04:01 +0300

    fix(*): add ci mode

--- src/shared/modules/config/config.validation.ts
+++ src/shared/modules/config/config.validation.ts
@@ -1,7 +1,7 @@
 import * as Joi from 'joi';
 
 export const CONFIG_VALIDATION_SCHEMA = Joi.object({
-  NODE_ENV: Joi.string().valid('development', 'production'),
+  NODE_ENV: Joi.string().valid('development', 'ci', 'production'),
   HTTP_PORT: Joi.number().port().required(),
   WS_PORT: Joi.number().port().required(),
   CONSUMER_ORIGIN: Joi.string().required(),
